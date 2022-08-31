30e5d554573d5ad0493b03b3f2f13a38a3149395
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-08-31 16:35:42 -0300

    Initial Commit

---
+++ application/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Componentes/Header'
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ application/src/Componentes/Header/index.js
@@ -0,0 +1,21 @@
+import './Header.css'
+import Brand from '../Brand'
+
+const Header = () => {
+    return(
+        <div class="Header">
+                <div class="Brand">
+                    <p>R|A|F|A|E|L</p>
+                </div>
+                
+                <div class="menu"> 
+                <a>Perfil</a> 
+                <a>Habilidades</a> 
+                <a>Experiência</a> 
+                <a>Contato</a>
+                </div>
+        </div>
+    )
+}
+
+export default Header;
